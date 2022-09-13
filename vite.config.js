d7f1c5586005bc1f3d3dd61cf42f74cf3f1d1d49
Ali-Mahmoud-Abdalluh <alimahmoudabdallaemam12@gmail.com>
2022-09-14 01:36:16 +0200

    dd

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  path: ".",
+  path: "name",
   plugins: [
     react(),
     viteCompression({
