9f1460070fed4770b97be0903eccb1f7f931b848
Ali-Mahmoud-Abdalluh <alimahmoudabdallaemam12@gmail.com>
2022-09-14 01:47:36 +0200

    ad

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  path: "name",
+  path: "https://github.com/Ali-Mahmoud-Abdalluh/bestpost-offical-webpage/",
   plugins: [
     react(),
     viteCompression({
