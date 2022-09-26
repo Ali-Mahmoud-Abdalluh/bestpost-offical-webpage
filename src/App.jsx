e8816af51c6b2509c71e99b397b8fd2f566e402c
Ali-Mahmoud-Abdalluh <alimahmoudabdallaemam12@gmail.com>
2022-09-26 23:57:16 +0200

    hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import loadable from '@loadable/component'
 const HomePage = loadable(() => import("./components/HomePage"))
 const Blog = loadable(() => import('./components/BlogPage'))
-
-
+import { Route, Routes } from "react-router-dom";
 
 const App = () => (
   <div>
     <Routes>
-      <Route exact path="/" element={<HomePage />} />
-      <Route exact path="/blog" element={<Blog />} />
+      <Route exact path="/bestpost-offical-webpage" element={<HomePage />} />
+      <Route exact path="/bestpost-offical-webpage/blog" element={<Blog />} />
     </Routes>
   </div>
 )
