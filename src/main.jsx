fccd08526b161b10c696a845d340e5cd06b0c13f
Ali-Mahmoud-Abdalluh <alimahmoudabdallaemam12@gmail.com>
2022-09-26 23:54:23 +0200

    change

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter >
-    <HashRouter>
-      <App />
-    </HashRouter>
+  <BrowserRouter basename='/bestpost-offical-webpage'>
+    <App />
   </BrowserRouter>
 )
